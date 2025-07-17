5b1634d9ade89378c3d1da6eb52a817b71579f8b
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-07-17 11:19:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelectorAll('.controls input');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const speedBar = document.querySelector('.speed-bar');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleUpdate() {
+  video[this.name] = this.value;
+
+  if (this.name === 'playbackRate') {
+    speedBar.textContent = `${this.value}×`;
+  }
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(btn => btn.addEventListener('click', skip));
+
+ranges.forEach(input => input.addEventListener('change', handleUpdate));
+ranges.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
